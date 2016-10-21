d3cad7b5213b2efc1f3d1b776d3f58602aef0012
Lisa Koshy <lisa.koshy@nokia.com>
2016-10-21 12:21:51 +0300

    File download windows desktop app

---
+++ filedownloader.py
@@ -0,0 +1,51 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+import sys
+import urllib.request
+
+
+class Downloader(QDialog):
+    def __init__(self):
+        QDialog.__init__(self)
+
+        layout = QVBoxLayout()
+
+        self.url = QLineEdit()
+        self.save_location = QLineEdit()
+        self.progress = QProgressBar()
+        download = QPushButton("Download")
+
+        self.url.setPlaceholderText("URL")
+        self.save_location.setPlaceholderText("File save location")
+        self.progress.setValue(0)
+        self.progress.setAlignment(Qt.AlignHCenter)
+
+        layout.addWidget(self.url)
+        layout.addWidget(self.save_location)
+        layout.addWidget(self.progress)
+        layout.addWidget(download)
+
+        self.setLayout(layout)
+        self.setWindowTitle("File Downloader Application")
+        self.setFocus()
+
+        download.clicked.connect(self.download)
+
+    # eventhandler
+    def download(self):
+        url = self.url.text()
+        save_location = self.save_location.text()
+        urllib.request.urlretrieve(url, save_location, self.report)
+
+
+    def report(self, blocknum, blocksize, totalsize):
+        readsofar = blocknum * blocksize
+        if totalsize > 0:
+            percent = readsofar * 100 / totalsize
+            self.progress.setValue(int(percent))
+
+
+app = QApplication(sys.argv)
+dl = Downloader()
+dl.show()
+app.exec_()
