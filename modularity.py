98419163f549e42e629c60a0ec23b0974add1646
Lisa Koshy <lisakoshy09@gmail.com>
2017-04-06 13:19:27 +0300

    A module demonstrating python modularity

---
+++ modularity.py
@@ -0,0 +1,47 @@
+import sys
+from urllib.request import urlopen
+
+
+def fetch_words(url):
+    """Fetch a list of words from a URL.
+
+    Args:
+        url: The URL of a utf-8 text document.
+
+    Returns:
+        A list of strings containing the words from the document
+    """
+    with urlopen(url) as story:
+        story_words = []
+        for line in story:
+            line_words = line.decode('utf-8').split()
+            for word in line_words:
+                story_words.append(word)
+
+    return story_words
+
+
+def print_items(items):
+    """Print items one per line
+
+    Args:
+        An iterable series of printable items
+    """
+    for item in items:
+        print(item)
+
+
+def main(url):
+    """Print each word from a text document from a URL.
+
+     Args:
+        url: The URL of a utf-8 text document.
+    """
+    # url = sys.argv[1]  # pass the url from the cmd line eg. python modularity <URL>
+
+    words = fetch_words(url)
+    print_items(words)
+
+
+if __name__ == '__main__':  # the module is executed directly as a program
+    main(sys.argv[1])
